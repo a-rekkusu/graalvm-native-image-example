346ea9df91fce4754113c3800437cbfb58297db6
Alexander Fischer <fischer.a@mailbox.org>
2020-08-05 10:58:48 +0200

    used owb's ClassLoaderProxyService.Spy

--- src/main/java/de/arekkusu/proxyGeneration/BuildTimeProxyGenerator.java
+++ src/main/java/de/arekkusu/proxyGeneration/BuildTimeProxyGenerator.java
@@ -2,20 +2,32 @@ package de.arekkusu.proxyGeneration;
 
 import org.apache.webbeans.config.WebBeansContext;
 import org.apache.webbeans.proxy.NormalScopeProxyFactory;
+import org.apache.webbeans.service.ClassLoaderProxyService;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class BuildTimeProxyGenerator
 {
     private List<Class> beansList = new ArrayList<>();
-    private NormalScopeProxyFactory pf = new NormalScopeProxyFactory(new WebBeansContext());
-    private List<Class> proxyList = new ArrayList<>();
+    private WebBeansContext context;
+    private NormalScopeProxyFactory pf;
+    private ClassLoaderProxyService.Spy spy;
+
+    public BuildTimeProxyGenerator()
+    {
+        this.context = new WebBeansContext();
+        this.context.getOpenWebBeansConfiguration().setProperty("org.apache.webbeans.proxy.useStaticNames", "true");
+        this.pf = new NormalScopeProxyFactory(context);
+        this.spy = new ClassLoaderProxyService.Spy(context);
+    }
 
     public static void main(String[] args) throws IOException
     {
         BuildTimeProxyGenerator proxyGenerator = new BuildTimeProxyGenerator();
+
         proxyGenerator.beansList.add(Foo.class);
         proxyGenerator.beansList.add(Foo2.class);
         proxyGenerator.beansList.add(Foo3.class);
@@ -23,21 +35,21 @@ public class BuildTimeProxyGenerator
 
         for (Class clazz : proxyGenerator.beansList)
         {
-            //NamingStrategy mitgeben - static names
-            Class proxy = proxyGenerator.pf.createProxyClass(ClassLoader.getSystemClassLoader(), clazz);
-            proxyGenerator.proxyList.add(proxy);
+            proxyGenerator.pf.createProxyClass(ClassLoader.getSystemClassLoader(), clazz);
+        }
 
+        for (Map.Entry<String, byte[]> entry : proxyGenerator.spy.getProxies().entrySet())
+        {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
             try
             {
                 ObjectOutputStream oos = new ObjectOutputStream(bos);
-                oos.writeObject(proxy);
+                oos.writeObject(entry.getValue());
                 oos.flush();
-                byte[] data = bos.toByteArray();
-                File file = new File("C:\\Users\\Alex\\Desktop\\" + proxy.getSimpleName() + ".class");
+                File file = new File("C:\\Users\\Alex\\Desktop\\" + entry.getKey() + ".class");
                 OutputStream out = new FileOutputStream(file);
-                out.write(data);
+                out.write(entry.getValue());
             }
             catch (IOException e)
             {
@@ -48,7 +60,7 @@ public class BuildTimeProxyGenerator
                 bos.close();
             }
 
-            System.out.println("Proxy generated: " + proxy.getSimpleName());
+            System.out.println("Proxy generated: " + entry.getKey());
         }
     }
 }
